3f0092e325857ef81f200939e1146c2273effa25
sapark22 <134351939+sapark22@users.noreply.github.com>
2025-03-29 15:40:10 +0900

    Title: 특이한 출력, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

--- 250329/특이한 출력/special-print.java
+++ 250329/특이한 출력/special-print.java
@@ -9,7 +9,7 @@ public class Main {
 
         for (int i = 1; i <= n ; i++) {
             for (int j = 1 ; j <= n ; j++) {
-                if ((i+j) == 4) {
+                if ((i+j) % 4 == 0) {
                     System.out.println("(" + i + ", " + j +")");
                 }
                 else {
