3a81b2aa3c7c90be42498f4fd8428e91caac81b0
sapark22 <134351939+sapark22@users.noreply.github.com>
2025-04-01 09:59:18 +0900

    Title: 알파벳 출력하기 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250401/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N =sc.nextInt();
+        int alpa = 65;
+
+        for(int i = 0; i<N; i++) {
+            for(int j = 0; j<i;j++){
+                System.out.print("  ");
+            }
+            for(int j = 1; j<=N-i;j++){
+                System.out.print((char)alpa+" ");
+                alpa ++;
+                if (alpa >90) alpa =65;
+            }
+            System.out.println();
+        }
+    }
+}
